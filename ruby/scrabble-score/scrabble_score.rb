1093f24fd9aad515f241ddc8f997077fb39d70d5
Gordon Chan <developer.gordon+github@gmail.com>
2016-01-22 23:39:28 +1300

    Ruby - scrabble-score

---
+++ ruby/scrabble-score/scrabble_score.rb
@@ -0,0 +1,29 @@
+class Scrabble
+  attr_reader :word
+
+  LETTER_SCORE = {
+    a: 1, e: 1, i: 1, o: 1, u: 1, l: 1, n: 1, r: 1, s: 1, t: 1,
+    d: 2, g: 2,
+    b: 3, c: 3, m: 3, p: 3,
+    f: 4, h: 4, v: 4, w: 4, y: 4,
+    k: 5,
+    j: 8, x: 8,
+    q: 10, z: 10,
+  }
+
+  def initialize(word)
+    @word = word&.strip
+  end
+
+  def score
+    return 0 if word.nil? || word.empty?
+
+    word.downcase.chars.
+      map { |letter| LETTER_SCORE[letter.to_sym] }.
+      inject(:+)
+  end
+
+  def self.score(word)
+    new(word).score
+  end
+end
