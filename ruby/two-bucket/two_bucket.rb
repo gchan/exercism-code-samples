7e86001e39c9f2ec87d662509a0f77b0bc1c60f7
Gordon Chan <developer.gordon+github@gmail.com>
2016-06-15 20:00:06 +1200

    Ruby - two-bucket

---
+++ ruby/two-bucket/two_bucket.rb
@@ -0,0 +1,125 @@
+class TwoBucket
+  attr_reader :goal_bucket, :other_bucket
+
+  def initialize(size_one, size_two, goal, first_bucket)
+    @size_one     = size_one
+    @size_two     = size_two
+    @goal         = goal
+    @first_bucket = first_bucket
+  end
+
+  def moves
+    empty_one
+    empty_two
+
+    moves_count = 0
+
+    while !solved?
+      move
+      moves_count +=1
+    end
+
+    set_solution
+    moves_count
+  end
+
+  private
+
+  def set_solution
+    if @bucket_one == @goal
+      @goal_bucket = 'one'
+      @other_bucket = @bucket_two
+    else
+      @goal_bucket = 'two'
+      @other_bucket = @bucket_one
+    end
+  end
+
+  def move
+    if @first_bucket == 'one'
+      start_from_one
+    else
+      start_from_two
+    end
+  end
+
+  def start_from_one
+    if one_empty?
+      fill_one
+    elsif two_full?
+      empty_two
+    else
+      fill_two_from_one
+    end
+  end
+
+  def start_from_two
+    if two_empty?
+      fill_two
+    elsif one_full?
+      empty_one
+    else
+      fill_one_from_two
+    end
+  end
+
+  def solved?
+    @bucket_one == @goal || @bucket_two == @goal
+  end
+
+  def one_empty?
+    @bucket_one == 0
+  end
+
+  def two_empty?
+    @bucket_two == 0
+  end
+
+  def one_full?
+    @bucket_one == @size_one
+  end
+
+  def two_full?
+    @bucket_two == @size_two
+  end
+
+  def fill_one
+    @bucket_one = @size_one
+  end
+
+  def fill_two
+    @bucket_two = @size_two
+  end
+
+  def empty_one
+    @bucket_one = 0
+  end
+
+  def empty_two
+    @bucket_two = 0
+  end
+
+  def fill_two_from_one
+    if @bucket_two + @bucket_one > @size_two
+      @bucket_one = @bucket_one + @bucket_two - @size_two
+      fill_two
+    else
+      @bucket_two += @bucket_one
+      empty_one
+    end
+  end
+
+  def fill_one_from_two
+    if @bucket_two + @bucket_one > @size_one
+      @bucket_two = @bucket_one + @bucket_two - @size_one
+      fill_one
+    else
+      @bucket_one += @bucket_two
+      empty_two
+    end
+  end
+end
+
+module BookKeeping
+  VERSION = 2
+end
---
+++ ruby/two-bucket/two_bucket_test.rb
@@ -0,0 +1,57 @@
+#!/usr/bin/env ruby
+gem 'minitest', '>= 5.0.0'
+require 'minitest/autorun'
+require_relative 'two_bucket'
+
+# Test data version:
+# deb225e Implement canonical dataset for scrabble-score problem (#255)
+
+class TwoBucketTest < Minitest::Test
+  def test_bucket_one_3_bucket_two_5_goal_1_starting_one
+    two_bucket = TwoBucket.new(3, 5, 1, 'one')
+    assert_equal 4, two_bucket.moves
+    assert_equal 'one', two_bucket.goal_bucket
+    assert_equal 5, two_bucket.other_bucket
+  end
+
+  def test_bucket_one_3_bucket_two_5_goal_1_starting_two
+    two_bucket = TwoBucket.new(3, 5, 1, 'two')
+    assert_equal 8, two_bucket.moves
+    assert_equal 'two', two_bucket.goal_bucket
+    assert_equal 3, two_bucket.other_bucket
+  end
+
+  def test_bucket_one_7_bucket_two_11_goal_2_starting_one
+    two_bucket = TwoBucket.new(7, 11, 2, 'one')
+    assert_equal 14, two_bucket.moves
+    assert_equal 'one', two_bucket.goal_bucket
+    assert_equal 11, two_bucket.other_bucket
+  end
+
+  def test_bucket_one_7_bucket_two_11_goal_2_starting_two
+    two_bucket = TwoBucket.new(7, 11, 2, 'two')
+    assert_equal 18, two_bucket.moves
+    assert_equal 'two', two_bucket.goal_bucket
+    assert_equal 7, two_bucket.other_bucket
+  end
+
+  # Problems in exercism evolve over time, as we find better ways to ask
+  # questions.
+  # The version number refers to the version of the problem you solved,
+  # not your solution.
+  #
+  # Define a constant named VERSION inside of the top level BookKeeping
+  # module.
+  #  In your file, it will look like this:
+  #
+  # module BookKeeping
+  #   VERSION = 1 # Where the version number matches the one in the test.
+  # end
+  #
+  # If you are curious, read more about constants on RubyDoc:
+  # http://ruby-doc.org/docs/ruby-doc-bundle/UsersGuide/rg/constants.html
+
+  def test_bookkeeping
+    assert_equal 2, BookKeeping::VERSION
+  end
+end
